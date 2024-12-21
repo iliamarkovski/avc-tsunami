fc5196d77c3214801fb7f97581c4690ca88602ab
Ilia Markovski <markovskiilia@gmail.com>
2024-12-21 18:09:06 +0200

    add dashboard nav page

---
+++ src/components/pages/Dashboard/DashboardNavPage.tsx
@@ -0,0 +1,50 @@
+import { Title } from '@/components';
+import { buttonVariants } from '@/components/ui/Button';
+import { cn } from '@/lib';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  // {
+  //   title: 'Volley Mania',
+  //   url: 'volleymania',
+  // },
+  // {
+  //   title: 'IVL',
+  //   url: 'ivl',
+  // },
+  // {
+  //   title: 'Тренировки',
+  //   url: 'trainings',
+  // },
+  {
+    title: 'Зали',
+    url: 'halls',
+  },
+  {
+    title: 'Отбори',
+    url: 'teams',
+  },
+  {
+    title: 'Състезатели',
+    url: 'members',
+  },
+];
+
+const DashboardNavPage = () => {
+  return (
+    <section className="flex flex-col gap-4">
+      <Title title="Управление" />
+      <nav className="flex flex-col gap-4">
+        {LINKS.map((link) => {
+          return (
+            <Link key={link.url} to={link.url} className={cn(buttonVariants({ variant: 'outline' }))}>
+              {link.title}
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export { DashboardNavPage };
