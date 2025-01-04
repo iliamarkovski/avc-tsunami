6d59405da503c098ca57c372d3d82c7947581abe
Ilia Markovski <markovskiilia@gmail.com>
2025-01-04 11:58:08 +0200

    improve useUserInfo hook

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -5,11 +5,10 @@ import { Roles } from '@/types';
 export const useUserInfo = (id?: string): { names: string; role: Roles; isActive: boolean } => {
   const { user } = useAuth();
   const { data } = useData();
-
   const { members, users } = data;
 
   const foundUser = id ? users.find((u) => u.id === id) : user;
-  const memberId = id ? foundUser?.memberId : user?.memberId;
+  const memberId = foundUser?.memberId;
 
   if (memberId === OTHER_VALUE) {
     return {
@@ -17,13 +16,13 @@ export const useUserInfo = (id?: string): { names: string; role: Roles; isActive
       role: foundUser?.role as Roles,
       isActive: false,
     };
-  } else {
-    const member = members.find((m) => m.id === memberId);
-
-    return {
-      names: member?.names || '',
-      role: member?.role as Roles,
-      isActive: member?.active || false,
-    };
   }
+
+  const member = members.find((m) => m.id === memberId);
+
+  return {
+    names: member?.names || '',
+    role: member?.role as Roles,
+    isActive: member?.active || false,
+  };
 };
