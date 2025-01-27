841100a52456d07df202db8b386438c848a6b9e1
Ilia Markovski <markovskiilia@gmail.com>
2025-01-27 13:35:34 +0200

    add additional regex to remove firebase id file name

--- src/lib/getFileNameFromUrl.ts
+++ src/lib/getFileNameFromUrl.ts
@@ -1,10 +1,17 @@
 export const getFileNameFromUrl = (url: string | undefined) => {
   if (!url) return url;
+
   // Use a regular expression to extract the part after "/statistics"
   const match = url.match(/\/statistics%2F([^?]+)/);
   if (match && match[1]) {
     // Decode the URL-encoded part to get the original file name
-    return decodeURIComponent(match[1]);
+    const decodedFileName = decodeURIComponent(match[1]);
+
+    // Remove the first part with the big number and hyphen
+    const fileNameWithoutPrefix = decodedFileName.replace(/^\d+-/, '');
+
+    return fileNameWithoutPrefix;
   }
+
   return null; // Return null if no match is found
 };
