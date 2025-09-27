8ac5af16d51a788dffddc0987df865c89903a33a
Ilia Markovski <markovskiilia@gmail.com>
2025-09-27 23:13:03 +0300

    Refactor DEFAULT_HALL_ID and SUPER_ADMIN_ID to use environment variables

--- src/constants/pageDefaults.ts
+++ src/constants/pageDefaults.ts
@@ -1,9 +1,9 @@
 import packageJson from '../../package.json';
 
 export const TEAM_NAME = 'AVC Tsunami';
-export const DEFAULT_HALL_ID = 'qwlU13Rm9seTENKgRBO5';
+export const DEFAULT_HALL_ID = import.meta.env.VITE_DEFAULT_HALL_ID;
 export const OTHER_VALUE = 'other';
-export const SUPER_ADMIN_ID = 'Jyp0ODc04sTGmAGvb7wnCoU8hfa2';
+export const SUPER_ADMIN_ID = import.meta.env.VITE_SUPER_ADMIN_ID;
 export const LOCAL_STORAGE_VIEW = 'selectedView';
 export const LOCAL_STORAGE_TAB = 'selectedTab';
 export const LATEST_VERSION = packageJson.version;
