51c9a507ac8ba89a480161f8a4e09a10f2b86ef2
Ilia Markovski <markovskiilia@gmail.com>
2025-10-04 14:22:45 +0300

    feat: add collapsible functionality to EventsMatchesList component

--- src/components/ui/EventsMatchesList.tsx
+++ src/components/ui/EventsMatchesList.tsx
@@ -11,7 +11,8 @@ import {
 import { useData } from '@/contexts';
 import { getDateByTimestamp, getNameById } from '@/lib';
 import { QueryKeys } from '@/types';
-import { Archive } from 'lucide-react';
+import { Archive, ArchiveX } from 'lucide-react';
+import { useState } from 'react';
 
 type Props = {
   queryKey: QueryKeys;
@@ -21,6 +22,7 @@ const EventsMatchesList = ({ queryKey }: Props) => {
   const { data } = useData();
   const { halls, teams } = data;
   const events = data[queryKey] as Matches[];
+  const [isOpen, setIsOpen] = useState(false);
 
   if (!events || events.length === 0) {
     return <NotFoundEvents />;
@@ -28,15 +30,16 @@ const EventsMatchesList = ({ queryKey }: Props) => {
 
   return (
     <div className="grid gap-4">
-      <Collapsible className="space-y-4">
-        <CollapsibleTrigger asChild>
-          <Button variant="outline">
-            <Archive /> Архив
-          </Button>
-        </CollapsibleTrigger>
+      <Collapsible open={isOpen} onOpenChange={setIsOpen} className="relative space-y-4">
         <CollapsibleContent className="grid gap-4">
           <ArchivedEvents queryKey={queryKey} halls={halls} teams={teams} />
         </CollapsibleContent>
+        <CollapsibleTrigger asChild>
+          <Button variant="outline" className="sticky bottom-4">
+            {isOpen ? <ArchiveX /> : <Archive />}
+            {isOpen ? 'Скрий архив' : 'Архив'}
+          </Button>
+        </CollapsibleTrigger>
       </Collapsible>
 
       {events.length === 0 ? (
