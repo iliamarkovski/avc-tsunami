9b9e948f35e112dc6c4ab3c29290a22bad708085
Ilia Markovski <markovskiilia@gmail.com>
2025-10-04 14:23:33 +0300

    refactor: replace useData with useLiveData for improved data fetching in EditTrainingPage

--- src/components/pages/Dashboard/Training/EditTrainingPage.tsx
+++ src/components/pages/Dashboard/Training/EditTrainingPage.tsx
@@ -1,7 +1,9 @@
 import { TrainingForm, FormCard, Training } from '@/components';
-import { useData } from '@/contexts';
+import { QUERY_KEYS } from '@/constants';
+import { useLiveData } from '@/hooks';
 import { getDataById } from '@/lib';
 import { QueryKeys } from '@/types';
+import { Loader2 } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 
 type Props = {
@@ -14,19 +16,19 @@ type Props = {
 const EditTrainingPage = ({ queryKey, title, description, parentUrl }: Props) => {
   const { id } = useParams();
 
-  const { data } = useData();
-  const event = getDataById<Training>(data[queryKey] as Training[], id);
+  const { data: training, loading: trainingLoading } = useLiveData<Training[]>(QUERY_KEYS.TRAINING);
+  const event = getDataById<Training>(training, id);
 
-  if (!event) {
-    return <p>Not Found</p>;
+  if (trainingLoading) {
+    return <Loader2 className="animate-spin" />;
   }
 
   return (
     <FormCard title={title} description={description}>
       <TrainingForm
-        dateTime={event.dateTime}
-        hall={event.hall}
-        message={event.message}
+        dateTime={event?.dateTime}
+        hall={event?.hall}
+        message={event?.message}
         id={id}
         parentUrl={parentUrl}
         queryKey={queryKey}
